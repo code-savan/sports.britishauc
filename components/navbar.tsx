548d3c394af6d2f04ca30c9e4dbe3d4bf9936d50
code-savan <eric.marvelboy@gmail.com>
2025-07-25 16:29:32 +0100

    updated navlink 2

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,7 +28,7 @@ const navigation: NavigationItem[] = [
     dropdown: [
       { name: 'Football Only', href: '/programmes/football-only' },
       { name: 'Football Plus Education', href: '/programmes/football-plus-education' },
-      { name: 'Short Football Programmes', href: '/programmes/pre-season-camp' },
+      { name: 'Short Football Programmes', href: '/programmes/short-football-programmes' },
       { name: 'Football Trials', href: '/programmes/football-trial' },
     //   { name: 'Football Plus Language', href: '/programmes/football-plus-language' },
     ]
