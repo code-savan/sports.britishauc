98d783efba183129865e7ffee253a58dc111092d
code-savan <eric.marvelboy@gmail.com>
2026-07-20 16:51:05 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  trailingSlash: false,
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
